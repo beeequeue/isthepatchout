1d91a40ea9ab399eca214194bbd07718d5bd6f0b
Adam <adam@haglund.dev>
2021-04-09 20:04:45 +0200

    add tracking

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+import * as Fathom from "fathom-client"
 import { createApp } from "vue"
 
 import App from "./App.vue"
@@ -5,3 +6,8 @@ import App from "./App.vue"
 import "modern-normalize"
 
 createApp(App).mount("#app")
+
+Fathom.load(import.meta.env.VITE_FATHOM_SITE_ID as string, {
+  url: "https://mammal.haglund.dev/script.js",
+  spa: "auto",
+})
