2b802f73858de650f1b6c5a37bb979b3c95bd595
Adam <adam@haglund.dev>
2021-04-09 18:50:54 +0200

    add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  root: true,
+  env: {
+    es2021: true,
+    browser: true,
+  },
+  extends: [
+    "plugin:@beequeue/base",
+    "plugin:@beequeue/vue",
+    "plugin:@beequeue/typescript",
+  ],
+}
--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,7 @@
 
 <script lang="ts">
 import { defineComponent } from "vue"
+
 import HelloWorld from "./components/HelloWorld.vue"
 
 export default defineComponent({
