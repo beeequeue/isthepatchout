bd448b18307e01970e246f3ef85c9174d6b5ef88
Adam <adam@haglund.dev>
2021-04-13 00:57:28 +0200

    use supabase client

--- api/check.ts
+++ api/check.ts
@@ -3,46 +3,38 @@ import { URLSearchParams } from "url"
 import Bottleneck from "bottleneck"
 import fetch from "make-fetch-happen"
 
+import { SupabaseClient } from "@supabase/supabase-js"
 import type { VercelRequest, VercelResponse } from "@vercel/node"
 
 import type { Patch } from "../src/types"
 
 const { CHECK_TOKEN, SUPABASE_SERVICE_KEY, VITE_SUPABASE_URL } = process.env
 
+const supabase = new SupabaseClient(
+  VITE_SUPABASE_URL as string,
+  SUPABASE_SERVICE_KEY as string,
+)
+
 const dotaApiScheduler = new Bottleneck({
   minTime: 1000,
   maxConcurrent: 3,
 })
 
-const authParam = new URLSearchParams({
-  apikey: SUPABASE_SERVICE_KEY as string,
-})
-
 const getPatchesToCheck = async (): Promise<Patch[] | null> => {
-  try {
-    const response = await fetch(
-      `${
-        VITE_SUPABASE_URL as string
-      }/rest/v1/patches?released=eq.false&${authParam.toString()}`,
-    )
-    const body = await response.json()
+  const response = await supabase.from<Patch>("patches").select().eq("released", false)
 
-    if (!response.ok) {
-      console.error(body)
-      return null
-    }
-
-    return body
-  } catch (err) {
-    console.error(err)
+  if (response.error != null) {
+    console.error(response.error)
     return null
   }
+
+  return response.data
 }
 
-const checkAndUpdatePatch = async (patch: string) => {
+const checkAndUpdatePatch = async (patch: Patch) => {
   const params = new URLSearchParams({
     language: "english",
-    version: patch,
+    version: patch.id,
   })
   const dataResponse = await fetch(
     `https://www.dota2.com/datafeed/patchnotes?${params.toString()}`,
@@ -60,19 +52,13 @@ const checkAndUpdatePatch = async (patch: string) => {
 
   if (data?.patch_timestamp == null) return
 
-  const updateResponse = await fetch(
-    `${
-      VITE_SUPABASE_URL as string
-    }/rest/v1/patches?id=eq.${patch}&${authParam.toString()}`,
-    {
-      method: "PATCH",
-      headers: { "content-type": "application/json" },
-      body: JSON.stringify({ released: true }),
-    },
-  )
+  const updateResponse = await supabase
+    .from<Patch>("patches")
+    .update({ released: true })
+    .eq("id", patch.id)
 
-  if (!updateResponse.ok) {
-    console.error(await updateResponse.text())
+  if (updateResponse.error != null) {
+    console.error(updateResponse.error)
   }
 }
 
@@ -84,9 +70,7 @@ const checkAndUpdatePatches = async () => {
   }
 
   await Promise.all(
-    patches.map((patch) =>
-      dotaApiScheduler.schedule(() => checkAndUpdatePatch(patch.id)),
-    ),
+    patches.map((patch) => dotaApiScheduler.schedule(() => checkAndUpdatePatch(patch))),
   )
 }
 
