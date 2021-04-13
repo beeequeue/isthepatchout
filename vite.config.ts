0e19bc0dc5c2b53b035e12e943b3adeb2ba903c7
Adam <adam@haglund.dev>
2021-04-13 11:28:19 +0200

    add pwa plugin, update favicon

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
 import { defineConfig } from "vite"
 import vue from "@vitejs/plugin-vue"
+import { VitePWA } from "vite-plugin-pwa"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    VitePWA({ registerType: "autoUpdate", manifest: { background_color: "#111" } }),
+  ],
 })
