9b8792a210ec147f0be2bf39ec8a4b41394cd9dd
Adam <adam@haglund.dev>
2021-04-19 18:30:50 +0200

    add temporary favicons & pwa config

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,23 @@ import { VitePWA } from "vite-plugin-pwa"
 export default defineConfig({
   plugins: [
     vue(),
-    VitePWA({ registerType: "autoUpdate", manifest: { background_color: "#111" } }),
+    VitePWA({
+      registerType: "autoUpdate",
+      manifest: {
+        name: "Is the Patch Out?",
+        short_name: "isthepatchout",
+        background_color: "#111",
+        theme_color: "#111",
+        icons: [
+          {
+            src: "/android-chrome-144x144.png?v=2",
+            sizes: "144x144",
+            type: "image/png",
+          },
+        ],
+        display: "standalone",
+        orientation: "portrait",
+      },
+    }),
   ],
 })
