0fb777af9ae2c6f3ff2863ec3c9d00defef3c7cb
Adam <adam@haglund.dev>
2021-04-23 19:43:00 +0200

    build source maps

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,9 @@ import { VitePWA } from "vite-plugin-pwa"
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    sourcemap: true,
+  },
   plugins: [
     vue(),
     VitePWA({
