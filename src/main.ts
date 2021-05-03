ed5dd68c24d99c2bc5e935b1731003a3ddce9da7
Adam <adam@haglund.dev>
2021-05-03 13:20:34 +0200

    add sentry to vue

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,24 @@
 import * as Fathom from "fathom-client"
 import { createApp } from "vue"
 
+import { captureException, init, setTag } from "@sentry/vue"
+
 import App from "./App.vue"
 
 import "modern-normalize"
 
-createApp(App).mount("#app")
+const app = createApp(App)
+
+init({
+  enabled: import.meta.env.PROD && !!import.meta.env.VITE_SENTRY_DSN,
+  dsn: import.meta.env.VITE_SENTRY_DSN as string | undefined,
+  environment: import.meta.env.VITE_VERCEL_MODE as string,
+})
+
+app.config.errorHandler = (error, _, info) => {
+  setTag("info", info)
+  captureException(error)
+}
 
 Fathom.load(import.meta.env.VITE_FATHOM_SITE_ID as string, {
   url: "https://mammal.haglund.dev/script.js",
@@ -17,3 +30,5 @@ if (import.meta.env.PROD && "serviceWorker" in navigator) {
     void navigator.serviceWorker.register("/sw.js", { scope: "/" })
   })
 }
+
+app.mount("#app")
