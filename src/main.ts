982de228e30e0bb7445c401e191b48770a898123
Adam <adam@haglund.dev>
2021-05-04 13:16:32 +0200

    Revert "lazy load sentry"
    This reverts commit f8bae10ec505f65bd9e733d05c00244de3ecf5ae.

--- src/main.ts
+++ src/main.ts
@@ -1,36 +1,34 @@
 import * as Fathom from "fathom-client"
 import { createApp } from "vue"
 
+import { captureException, init, setTag } from "@sentry/vue"
+
 import App from "./App.vue"
 
 import "modern-normalize"
 
-void (async () => {
-  const Sentry = await import(/* chunkname: "test" */ "@sentry/vue")
+const app = createApp(App)
 
-  const app = createApp(App)
+init({
+  enabled: import.meta.env.PROD && !!import.meta.env.VITE_SENTRY_DSN,
+  dsn: import.meta.env.VITE_SENTRY_DSN as string | undefined,
+  environment: import.meta.env.VITE_VERCEL_MODE as string,
+})
 
-  Sentry.init({
-    enabled: import.meta.env.PROD && !!import.meta.env.VITE_SENTRY_DSN,
-    dsn: import.meta.env.VITE_SENTRY_DSN as string | undefined,
-    environment: import.meta.env.VITE_VERCEL_MODE as string,
+app.config.errorHandler = (error, _, info) => {
+  setTag("info", info)
+  captureException(error)
+}
+
+Fathom.load(import.meta.env.VITE_FATHOM_SITE_ID as string, {
+  url: "https://mammal.haglund.dev/script.js",
+  spa: "auto",
+})
+
+if (import.meta.env.PROD && "serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    void navigator.serviceWorker.register("/sw.js", { scope: "/" })
   })
+}
 
-  app.config.errorHandler = (error, _, info) => {
-    Sentry.setTag("info", info)
-    Sentry.captureException(error)
-  }
-
-  Fathom.load(import.meta.env.VITE_FATHOM_SITE_ID as string, {
-    url: "https://mammal.haglund.dev/script.js",
-    spa: "auto",
-  })
-
-  if (import.meta.env.PROD && "serviceWorker" in navigator) {
-    window.addEventListener("load", () => {
-      void navigator.serviceWorker.register("/sw.js", { scope: "/" })
-    })
-  }
-
-  app.mount("#app")
-})()
+app.mount("#app")
