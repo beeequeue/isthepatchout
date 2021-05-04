de83a9cc223261d1f799e68d5fa554b676141222
Adam <adam@haglund.dev>
2021-05-04 12:34:18 +0200

    lazy load sentry

--- src/main.ts
+++ src/main.ts
@@ -1,34 +1,36 @@
 import * as Fathom from "fathom-client"
 import { createApp } from "vue"
 
-import { captureException, init, setTag } from "@sentry/vue"
-
 import App from "./App.vue"
 
 import "modern-normalize"
 
-const app = createApp(App)
+void (async () => {
+  const Sentry = await import(/* chunkname: "test" */ "@sentry/vue")
 
-init({
-  enabled: import.meta.env.PROD && !!import.meta.env.VITE_SENTRY_DSN,
-  dsn: import.meta.env.VITE_SENTRY_DSN as string | undefined,
-  environment: import.meta.env.VITE_VERCEL_MODE as string,
-})
+  const app = createApp(App)
 
-app.config.errorHandler = (error, _, info) => {
-  setTag("info", info)
-  captureException(error)
-}
-
-Fathom.load(import.meta.env.VITE_FATHOM_SITE_ID as string, {
-  url: "https://mammal.haglund.dev/script.js",
-  spa: "auto",
-})
-
-if (import.meta.env.PROD && "serviceWorker" in navigator) {
-  window.addEventListener("load", () => {
-    void navigator.serviceWorker.register("/sw.js", { scope: "/" })
+  Sentry.init({
+    enabled: import.meta.env.PROD && !!import.meta.env.VITE_SENTRY_DSN,
+    dsn: import.meta.env.VITE_SENTRY_DSN as string | undefined,
+    environment: import.meta.env.VITE_VERCEL_MODE as string,
   })
-}
 
-app.mount("#app")
+  app.config.errorHandler = (error, _, info) => {
+    Sentry.setTag("info", info)
+    Sentry.captureException(error)
+  }
+
+  Fathom.load(import.meta.env.VITE_FATHOM_SITE_ID as string, {
+    url: "https://mammal.haglund.dev/script.js",
+    spa: "auto",
+  })
+
+  if (import.meta.env.PROD && "serviceWorker" in navigator) {
+    window.addEventListener("load", () => {
+      void navigator.serviceWorker.register("/sw.js", { scope: "/" })
+    })
+  }
+
+  app.mount("#app")
+})()
