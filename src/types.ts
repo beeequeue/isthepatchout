1a093b39f57df133c5f87f849dff9e2fd2cb419f
Adam <adam@haglund.dev>
2021-06-03 19:59:14 +0200

    remove generated types

--- src/types.ts
+++ src/types.ts
@@ -1,7 +1,23 @@
-import { definitions } from "./generated"
+export type Patch = {
+  /**
+   * The patch ID, e.g. `7.00`, `7.28c`, `7.29`
+   *
+   * Note:
+   * This is a Primary Key.<pk/>
+   */
+  id: string
+  links: string[]
+  releasedAt?: string
+  number: number
+}
 
-export type Patch = definitions["patches"]
-export type PushSubscription = definitions["subscriptions"]
+export type PushSubscription = {
+  endpoint: string
+  createdAt: string
+  auth: string
+  p256dh: string
+  environment: string
+}
 
 export type PushEventPatch = Patch & {
   type: "patch"
