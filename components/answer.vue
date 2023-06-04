2b55e47a0391a11b0ade06aff6eb9878fc7f0a9e
Adam <adam@haglund.dev>
2023-06-04 18:04:37 +0200

    fix type error

--- components/answer.vue
+++ components/answer.vue
@@ -1,15 +1,18 @@
+<!-- eslint-disable vue/attribute-hyphenation -->
+
 <template>
   <div class="flex relative align-center font-serif pointer-events-none">
     <transition
       mode="out-in"
-      class="duration-250"
-      enter-active-class="ease-out"
-      leave-active-class="ease-in"
-      enter-from-class="transform -translate-x-full opacity-0"
-      leave-to-class="transform translate-x-full opacity-0"
+      enterActiveClass="ease-out"
+      leaveActiveClass="ease-in"
+      enterFromClass="transform -translate-x-full opacity-0"
+      leaveToClass="transform translate-x-full opacity-0"
     >
-      <div v-if="recentlyReleased" key="true" class="text-9xl md:text-12xl">Yes!</div>
-      <div v-else key="false" class="text-10xl md:text-12xl">No.</div>
+      <div v-if="recentlyReleased" key="true" class="text-9xl md:text-12xl duration-250">
+        Yes!
+      </div>
+      <div v-else key="false" class="text-10xl md:text-12xl duration-250">No.</div>
     </transition>
   </div>
 </template>
