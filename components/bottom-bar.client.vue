ba97efc04f3f2cd30a139ed3ddf3d759707ccbd7
Adam <adam@haglund.dev>
2025-05-22 22:33:59 +0200

    update how bottom bar looks

--- components/bottom-bar.client.vue
+++ components/bottom-bar.client.vue
@@ -1,16 +1,7 @@
 <template>
   <nav
-    class="w-1/1 absolute bottom-0 mt-auto flex flex-col items-center justify-center bg-gradient-to-br from-gray-900/75 to-gray-900/20 p-1 shadow-lg transition-all duration-500 md:mb-5 md:w-auto md:rounded-lg"
-    :class="{ open }"
+    class="absolute bottom-0 mb-5 mt-auto flex w-auto flex-col items-center justify-center rounded-lg bg-gradient-to-br from-gray-800/40 to-slate-900/40 p-1 shadow-lg transition-all duration-500"
   >
-    <Clickable
-      v-if="isMobile"
-      class="absolute right-0 top-0 -translate-y-full transform !rounded-full"
-      @click="open = !open"
-    >
-      <Icon name="pajamas:settings" class="h-8 w-8 p-0" />
-    </Clickable>
-
     <Notifications />
 
     <DiscordLink />
@@ -23,14 +14,4 @@
 import DiscordLink from "./bottom-bar/discord-link.vue"
 import GitHubLink from "./bottom-bar/github-link.vue"
 import Notifications from "./bottom-bar/notifications.client.vue"
-
-const windowSize = useWindowSize()
-const isMobile = computed(() => windowSize.width.value < 768)
-const open = ref(!isMobile.value)
 </script>
-
-<style scoped>
-nav:not(.open) {
-  transform: translateY(100%);
-}
-</style>
