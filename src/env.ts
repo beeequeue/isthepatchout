d108f9d50fac97a6b317fd0d5fee66b2815f7bd3
bq <adam@haglund.dev>
2026-07-28 00:08:25 +0200

    fix it in a better way

--- src/env.ts
+++ src/env.ts
@@ -3,35 +3,33 @@ import * as v from "valibot"
 
 import { building } from "$app/env"
 
-const vapidPublicKeySchema = v.optional(
-  v.pipe(
-    v.string(),
-    v.transform((data) => Uint8Array.fromBase64(data, { alphabet: "base64url" })),
-    v.check((buffer) => buffer.byteLength === 65, "Public key is not 65 bytes long."),
-  ),
+const buildingSchema = v.optional(v.any())
+
+const vapidPublicKeySchema = v.pipe(
+  v.string(),
+  v.transform((data) => Uint8Array.fromBase64(data, { alphabet: "base64url" })),
+  v.check((buffer) => buffer.byteLength === 65, "Public key is not 65 bytes long."),
 )
 
-const vapidPrivateKeySchema = v.optional(
-  v.pipe(
-    v.string(),
-    v.transform((data) => Uint8Array.fromBase64(data, { alphabet: "base64url" })),
-    v.check((buffer) => buffer.byteLength === 32, "Private key is not 32 bytes long."),
-  ),
+const vapidPrivateKeySchema = v.pipe(
+  v.string(),
+  v.transform((data) => Uint8Array.fromBase64(data, { alphabet: "base64url" })),
+  v.check((buffer) => buffer.byteLength === 32, "Private key is not 32 bytes long."),
 )
 
 export const variables = defineEnvVars({
   DISCORD_CLIENT_ID: {
-    schema: v.optional(v.string()),
+    schema: !building ? v.string() : buildingSchema,
   },
   DISCORD_CLIENT_SECRET: {
-    schema: v.optional(v.string()),
+    schema: !building ? v.string() : buildingSchema,
   },
 
   VAPID_PUBLIC_KEY: {
     public: true,
-    schema: !building ? vapidPublicKeySchema : undefined,
+    schema: !building ? vapidPublicKeySchema : buildingSchema,
   },
   VAPID_PRIVATE_KEY: {
-    schema: !building ? vapidPrivateKeySchema : undefined,
+    schema: !building ? vapidPrivateKeySchema : buildingSchema,
   },
 })
